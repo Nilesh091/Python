688f60a85eb6d1e00cc8572f4e7ab6bfffc94089
nileshranjan <ranjannilesh40@gmail.com>
2023-05-04 21:54:41 +0530

    Nil

---
+++ f1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+import numpy as np
+vectorAsArray=np.array([1,2,3,4,5])
+rowVector=np.array([[1,2,3,4,5] ])
+colVector=np.array([[1],[2],[3],[4],[5]])
+print(vectorAsArray)
+print(rowVector)
+print(colVector)
